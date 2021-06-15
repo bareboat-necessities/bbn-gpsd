cf3e2930540187fb6d8b1ba7634563a0e5d8be4b
mgrouch <mgrushinskiy@gmail.com>
2021-06-15 19:30:18 -0400

    bbn-gpsd

--- build-deb.sh
+++ build-deb.sh
@@ -3,9 +3,12 @@
 wget http://deb.debian.org/debian/pool/main/g/gpsd/gpsd_3.22-3.debian.tar.xz
 wget http://deb.debian.org/debian/pool/main/g/gpsd/gpsd_3.22.orig.tar.xz
 
-sudo apt-get -y install devscripts build-essential lintian wget xz-utils
+apt-get update  -y -q
+apt-get install -y -q wget xz-utils gnupg ca-certificates
 
-sudo apt-get -y install python3-all-dev python3-all-dbg xsltproc docbook-xsl \
+apt-get -y install devscripts build-essential lintian
+
+apt-get -y install python3-all-dev python3-all-dbg xsltproc docbook-xsl \
   docbook-xml libncurses-dev libdbus-glib-1-dev makedev python3-matplotlib \
   python3-gps qtbase5-dev scons dh-buildinfo dh-apparmor dh-exec pps-tools \
   asciidoc asciidoctor libsystemd-dev
